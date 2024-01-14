5b5d6fd7f5d1b9f6ad24f40218a634de3bd59baa
Nawodya Ishan <nawodyain@gmail.com>
2024-01-15 00:58:37 +0530

    feat: setup tests

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -1,11 +1,10 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
-    constructor() ERC721() {
+    // State Variables
 
-    }
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Accessing Environment variables
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+if (!INFURA_API_KEY) throw new Error("Environment variable INFURA_API_KEY is not set!")
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+if (!SEPOLIA_RPC_URL) throw new Error("Environment variable SEPOLIA_RPC_URL is not set!")
+
+const AVALANCHE_FUJI_RPC_URL = process.env.AVALANCHE_FUJI_RPC_URL
+if (!AVALANCHE_FUJI_RPC_URL) throw new Error("Environment variable AVALANCHE_FUJI_RPC_URL is not set!")
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+if (!PRIVATE_KEY) throw new Error("Environment variable PRIVATE_KEY is not set!")
+
 
 const config: HardhatUserConfig = {
     solidity: "0.8.20",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111
+        },
+    }
 };
 
 export default config;
