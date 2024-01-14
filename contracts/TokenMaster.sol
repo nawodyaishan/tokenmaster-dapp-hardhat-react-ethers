add3e66f90991f8c795c781b87c6f4c071d3b32d
Nawodya Ishan <nawodyain@gmail.com>
2024-01-15 00:39:33 +0530

    feat: add import ERC721

---
+++ contracts/TokenMaster.sol
@@ -0,0 +1,11 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    constructor() ERC721() {
+
+    }
+}
